364d485030526f926e2a69c24319a21057cf5641
Geoffery Koranteng <74549786+access6080@users.noreply.github.com>
2023-11-04 00:54:42 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ const root = document.getElementById("root");
 const init = async () => {
     //fetch data
     try {
-        const data = await fetch("http://localhost:8080/api/v1/name/Geoffery")
+        const data = await fetch("/api/v1/name/Geoffery")
         root.innerText =  await data.text();
     } catch (e) {
 
